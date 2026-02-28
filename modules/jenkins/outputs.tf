c83174baab94ec69c0e0363be00377addbab8b4a
Felix <phanthanh93@gmail.com>
2026-02-28 13:05:04 +0100

    add comment to explain the role of output file

--- modules/jenkins/outputs.tf
+++ modules/jenkins/outputs.tf
@@ -1,3 +1,5 @@
+// After running "terraform apply". It will print instance_id and public_ip
+// so that root module can access these value
 output "instance_id" {
   value = aws_instance.jenkins.id
 }
